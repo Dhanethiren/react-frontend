303130abd0a4c0b48e0b2273a31144e3b02c9cee
Dhanethiren <dhanethiren@gmail.com>
2025-03-14 10:22:27 +0530

    Update StudentPage.js

--- src/components/StudentPage.js
+++ src/components/StudentPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Profile from './Profile';
+import Logout from './Logout';
 
 const StudentPage = () => {
   return (
     <div className="student-container">
       <h2>Welcome to the Student Dashboard!</h2>
+      <Profile />
+      <Logout />
     </div>
   );
 };
